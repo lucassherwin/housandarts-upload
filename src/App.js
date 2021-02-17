7b17ad52d39337f5cc5f07b3a60b55e0ea37d594
lucassherwin <lksherwin@gmail.com>
2021-02-17 17:53:55 -0500

    all posts are now rendered

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react';
 import Upload from './components/Upload.js'
+import { db } from './firebase/firebase';
 
 export default function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection('posts').get().then((snapshot) => {
+      snapshot.docs.forEach(doc => {
+        let post = doc.data();
+        setPosts(prevState => [...prevState, post]);
+      })
+    })
+  }, [])
   return (
     <div>
       <Upload />
+      {
+        posts.map((post) => (
+          <img src={post.src} alt={post.title} height='200px' />
+        ))
+      }
     </div>
   )
 }
