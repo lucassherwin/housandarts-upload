d2dccd265069ae4a4af7fef8b80ac511f2f3bd34
lucassherwin <lksherwin@gmail.com>
2021-02-17 16:40:48 -0500

    cleaned up code

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,18 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/storage';
-// import withFirebaseAuth from 'react-with-firebase-auth'
 import 'firebase/auth';
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAdhebuUa1UcV0V1KTOmlO2Dghc9M-8CjM",
-//   authDomain: "housandarts-96836.firebaseapp.com",
-//   projectId: "housandarts-96836",
-//   storageBucket: "housandarts-96836.appspot.com",
-//   messagingSenderId: "739415989440",
-//   appId: "1:739415989440:web:23813fc45410d4c1552125",
-//   measurementId: "G-FYXSW82JZS"
-// };
 
 const firebaseConfig = {
   apiKey: "AIzaSyAdhebuUa1UcV0V1KTOmlO2Dghc9M-8CjM",
@@ -25,7 +14,6 @@ const firebaseConfig = {
   measurementId: "G-FDWVYPR6C7"
 };
 
-
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 // Initialize storage
@@ -34,7 +22,6 @@ const storage = firebase.storage();
 const db = firebase.firestore();
 // Initialize Analytics
 // firebase.analytics();
-
 
 export  {
   db, storage, firebase as default
